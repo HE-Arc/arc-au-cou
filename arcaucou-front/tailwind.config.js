6dca7d1006fe4a29becf8d7f7b67fbbb385be3b5
Tino3210 <izzovalentino0@gmail.com>
2022-03-10 20:59:29 +0100

    add new color for light and dark

---
+++ arcaucou-front/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    colors: {
+      'd-background': '#1a1a1a',
+      'd-background-light': '#1F5F5B',
+      'd-background-darker': '#1c1917',
+      'd-background-night': '#0d1a23',
+      'd-border': '#45886f',
+      'd-text': '#fefffe',
+      'l-background': '#e5f9ff',
+      'l-text': '#2a464e',
+      'green-gemme': '#4ce6a7',
+    },
+  },
+}
