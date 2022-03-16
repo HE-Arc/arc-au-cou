fd74fc46595bd20d245d0288142d937319b6d201
Tino3210 <izzovalentino0@gmail.com>
2022-03-16 20:14:03 +0100

    refactor: use grid to display sudokugrid

---
+++ arcaucou-front/components/Square.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center bg-d-border">
+    {{value}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+  }
+}
+</script>
---
+++ arcaucou-front/components/SudokuGrid.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="grid grid-cols-3 gap-4 mx-auto w-1/2 dark:bg-d-background-light">
+    <div v-for="index in 9" :key="index" >
+      <Block :class="[{ 'mx-px': index === 2}, { 'm-px': index === 5 }, { 'my-px': index === 4 }, { 'my-px': index === 6 }, { 'mx-px': index === 8 }]"/>
+    </div>
+  </diV>
+</template>
+
+<script>
+
+export default {
+  props: {
+    gridStart: Array,
+  }
+}
+</script>
+
