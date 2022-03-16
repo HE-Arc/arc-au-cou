792dccf19704594456a94da3e471e2cf1c67bae3
Lopes Da Silva Diogo <diogo.lopesdasilva@he-arc.ch>
2022-03-16 08:45:51 +0100

    changed database to mysql and added user table

--- arcaucou/arcaucouapp/views.py
+++ arcaucou/arcaucouapp/views.py
@@ -12,12 +12,3 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
