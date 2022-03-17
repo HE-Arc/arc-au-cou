e6d5f2409f7eedcbfe22313026ed9be5e694a477
Lopes Da Silva Diogo <diogo.lopesdasilva@he-arc.ch>
2022-03-17 14:24:40 +0100

    database tables

--- arcaucou/arcaucouapp/serializers.py
+++ arcaucou/arcaucouapp/serializers.py
@@ -1,11 +1,20 @@
-from django.contrib.auth.models import User, Group
+from arcaucouapp.models import User, Group
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['url', 'username', 'email']
+        
+class UserCreateSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        instance = User.objects.create_user(**validated_data)
+        return instance
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
--- arcaucou/arcaucouapp/views.py
+++ arcaucou/arcaucouapp/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from arcaucouapp.serializers import UserSerializer, GroupSerializer
+from arcaucouapp.models import User, Group
+from rest_framework import viewsets, permissions
+from arcaucouapp.serializers import UserSerializer, GroupSerializer, UserCreateSerializer
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
