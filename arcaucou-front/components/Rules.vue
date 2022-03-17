09f6a3f39292e1fdf5d96e417dc47242c906c5cb
Tino3210 <izzovalentino0@gmail.com>
2022-03-17 14:07:53 +0100

    feat<#5>: add timer, moment js and rules

---
+++ arcaucou-front/components/Rules.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mx-auto w-1/2">
+    <h1>Règles - Arc Au Cou</h1>
+    <p>Arc Au Cou vous propose de résoudre un nouveau sudoku par jour !</p>
+    <p>Pour résoudre le sudoku au-dessus cliquer sur une des cases et utiliser votre clavier pour remplir ou remplacer par un chiffre.</p>
+  </diV>
+</template>
--- arcaucou-front/pages/index.vue
+++ arcaucou-front/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-
   <main class='bg-l-background min-h-screen dark:bg-d-background text-l-text dark:text-d-text'>
-    <SudokuGrid :gridStart="gridStart"/>
+    <Timer :running=true class="mb-4"/>
+    <div class="mx-auto w-1/2 text-center mb-4">
+      <button class="text-gray-800 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-green-gemme">Commencer</button>
+    </diV>
+    <SudokuGrid class="mb-10" :gridStart="gridStart"/>
+    <Rules/>
   </main>
 </template>
 
@@ -19,7 +23,8 @@ export default {
               [0,8,0,0,5,6,0,0,0],
               [1,3,0,0,0,0,0,0,5],
               [0,0,0,0,0,0,2,0,6],
-              [2,5,0,0,7,4,3,0,0]]
+              [2,5,0,0,7,4,3,0,0]],
+    started: false
     }
   },
 }
