36009699ff71e8469a3cc9252436e324e8123005
Tino3210 <izzovalentino0@gmail.com>
2022-03-24 16:44:52 +0100

    style: sudoku and auth

--- arcaucou-front/components/Block.vue
+++ arcaucou-front/components/Block.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="grid grid-cols-3 gap-1 content-center max-w-xs">
+  <div class="grid grid-cols-3 gap-0 justify-center max-w-xs">
     <div class="" v-for="(cell,index) in values" :key="index">
-      <Square class="p-1" :cell="cell" :posX="idBlock" :posY="index"/>
+      <Square class="p-1 mx-auto" :cell="cell" :posX="idBlock" :posY="index"/>
     </div>
   </div>
 </template>
--- arcaucou-front/pages/register.vue
+++ arcaucou-front/pages/register.vue
@@ -2,31 +2,31 @@
   <div class="font-sans bg-l-background dark:bg-d-background dark:text-d-text">
     <div class="relative min-h-screen flex flex-col sm:justify-center items-center">
         <div class="relative sm:max-w-sm w-full">
-            <div class="card dark:bg-green-gemme shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-6"></div>
-            <div class="card dark:bg-green-gemme shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
-            <div class="relative w-full rounded-3xl  px-6 py-4 dark:bg-d-background-night shadow-md">
-                <h1 class="block mt-3 text-xl text-gray-700 text-center font-semibold">
+            <div class="card bg-l-10 dark:bg-d-10 shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-6"></div>
+            <div class="card bg-l-10 dark:bg-d-10 shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
+            <div class="relative w-full rounded-3xl  px-6 py-4 bg-l-60 dark:bg-d-60 shadow-md">
+                <h1 class="block mt-3 text-xl text-center font-semibold">
                     Register
                 </h1>
                 <form @submit.prevent="handleRegister" class="mt-10">
                     <div>
-                        <input type="text" placeholder="Username" v-model="userData.username" class="mt-1 pl-3 block w-full border-none bg-gray-100 dark:bg-d-background h-11 rounded-xl shadow-lg focus:border-green-gemme">
-                    </div>
-
-                    <div>
-                        <input type="email" placeholder="Email" v-model="userData.email" class="mt-1 pl-3 block w-full border-none bg-gray-100 dark:bg-d-background h-11 rounded-xl shadow-lg focus:border-green-gemme">
+                        <input type="text" placeholder="Username" v-model="userData.username" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
                     </div>
 
                     <div class="mt-7">
-                        <input type="password" placeholder="Password" v-model="userData.password" class="mt-1 pl-3 block w-full border-none bg-gray-100 dark:bg-d-background h-11 rounded-xl shadow-lg focus:border-green-gemme">
+                        <input type="email" placeholder="Email" v-model="userData.email" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
                     </div>
 
                     <div class="mt-7">
-                        <input type="password" placeholder="Confirm password" v-model="userData.password2" class="mt-1 pl-3 block w-full border-none bg-gray-100 dark:bg-d-background h-11 rounded-xl shadow-lg focus:border-green-gemme">
+                        <input type="password" placeholder="Password" v-model="userData.password" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
+                    </div>
+
+                    <div class="mt">
+                        <input type="password" placeholder="Confirm password" v-model="userData.password2" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
                     </div>
 
                     <div class="mt-7">
-                        <button class="mb-4 border-green-gemme border-2 bottom-2 w-full py-3 rounded-xl shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                        <button class="mb-4 bg-l-background dark:bg-d-30 border-l-10 dark:border-d-10 border-2 bottom-2 w-full py-3 rounded-xl shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
                             Register
                         </button>
                     </div>
@@ -41,7 +41,12 @@
 <script>
 export default {
   layout: 'main',
-  name: 'Arc Au Cou - Register',
+  name: 'register',
+  head() {
+    return {
+      title: 'Arc Au Cou - Register'
+    };
+  },
   data: function(){
     return {
       userData:{
