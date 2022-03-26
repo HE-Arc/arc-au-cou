12bda2892e1b15006faa9f95327c23513b1c186c
Tino3210 <izzovalentino0@gmail.com>
2022-03-26 09:57:31 +0100

    feat: forms to create or join groups

--- arcaucou-front/pages/groups.vue
+++ arcaucou-front/pages/groups.vue
@@ -1,12 +1,46 @@
 <template>
-  <main class='pt-10 bg-l-background min-h-screen dark:bg-d-background text-l-text dark:text-d-text'>
-    <div class="flex">
-      <div class="mt-7">
-        <input type="text" placeholder="Nom du group" class="mt-1 pl-3 block w-full border-none bg-gray-100 dark:bg-d-background h-11 rounded-xl shadow-lg focus:border-green-gemme">
+  <main class='mt-16 bg-l-background min-h-screen dark:bg-d-background text-l-text dark:text-d-text fixed w-full h-full top-0 left-0 z-0'>
+    <div v-if="hasGroup" class="mx-auto w-1/4">
+      <div class="grid grid-cols-2 mx-auto mt-10 gap-y-8">
+        <div class="text-center col-span-2">
+          <h2 class="text-2xl">Vos groupes</h2>
+        </div>
+        <div class="">
+          <p>Nom</p>
+        </div>
+        <div class="text-center">
+          <p>Quitter</p>
+        </div>
+      </div>
+      <div class="grid grid-cols-2 mx-auto" v-for="name in groupsList" :key="name">
+        <p class="my-auto text-xl">{{name}}</p>
+        <div  class="w-full">
+          <svg @click="handleQuit(name)" xmlns="http://www.w3.org/2000/svg" class="mx-auto h-10 w-10 cursor-pointer hover:text-red dark:hover:text-red dark:text-d-10" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div class="mt-20 mx-auto w-1/4">
+      <h1 class="text-2xl">Rejoindre ou créer un groupe</h1>
+      <br>
+      <p>Rejoindre un groupe, vous permet de vous ... avec vos amis. Un classement spécial est reservé aux groupes en dessous du classement général.</p>
+      <br>
+      <div class="mt-4">
+          <input type="text" v-model="groupData.name" placeholder="Nom du groupe" class="mt-1 pl-3 block w-full border-l-10 border-2 bg-l-background dark:border-d-30 dark:bg-d-30 h-11 rounded-xl shadow-lg dark:focus:border-d-10 focus:border-d-10">
       </div>
 
       <div class="mt-7">
-        <input type="password" placeholder="Mot de passe" class="mt-1 pl-3 block w-full border-none bg-gray-100 dark:bg-d-background h-11 rounded-xl shadow-lg focus:border-green-gemme">
+          <input type="password" v-model="groupData.password" placeholder="Mot de passe" class="mt-1 pl-3 block w-full border-l-10 border-2 dark:border-d-30 bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
+      </div>
+
+      <div class="mt-7 flex">
+          <button @click="handleCreate" class="w-1/2 bg-l-background dark:bg-d-30 border-l-10 dark:border-d-10 border-2 bottom-2 py-3 rounded-xl shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+              Créer
+          </button>
+          <button @click="handleJoin" class="w-full ml-5 bg-l-background dark:bg-d-30 border-l-10 dark:border-d-10 border-2 bottom-2 py-3 rounded-xl shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+              Rejoindre
+          </button>
       </div>
     </diV>
   </main>
@@ -23,7 +57,24 @@ export default {
   },
   data: function() {
     return {
+      groupsList: ['Les arbres', 'Les boufs'],
+      groupData: {
+        name: '',
+        password: ''
+      },
+      hasGroup: true,
     }
   },
+  methods:{
+    handleCreate: function(){
+      console.log("Create");
+    },
+    handleJoin: function(){
+      console.log("Join");
+    },
+    handleQuit: function(name){
+      console.log(name);
+    }
+  }
 }
 </script>
