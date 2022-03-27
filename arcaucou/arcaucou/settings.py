b31b6a8ce7c68250440df96e84ca1ad7c418b3a5
Bruno Costa <bruno.costa@he-arc.ch>
2022-03-27 15:14:44 +0200

    CSS

--- arcaucou/arcaucou/settings.py
+++ arcaucou/arcaucou/settings.py
@@ -142,7 +142,7 @@ CRONJOBS = [
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = '/var/www/arcaucou/'
-STATIC_URL = '/var/www/arcaucou/shared/env/lib/python3.8/site-packages/rest_framework/static/'
+STATIC_URL = '/var/www/arcaucou/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
