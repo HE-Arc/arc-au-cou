69d249c5361346b10c80aab06cd16b7bb92f9517
Bruno Costa <bruno.costa@he-arc.ch>
2022-03-27 15:29:09 +0200

    Static

--- arcaucou/arcaucou/settings.py
+++ arcaucou/arcaucou/settings.py
@@ -142,7 +142,7 @@ CRONJOBS = [
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = '/var/www/arcaucou/current/arcaucou/static/'
-STATIC_URL = '/var/www/arcaucou/current/arcaucou/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
