a88db183660c72ff067995a94428e47456b41768
Tino3210 <izzovalentino0@gmail.com>
2022-03-29 18:37:20 +0200

    refactor: trad

--- arcaucou-front/pages/register.vue
+++ arcaucou-front/pages/register.vue
@@ -10,7 +10,7 @@
                 </h1>
                 <form @submit.prevent="handleRegister" class="mt-10">
                     <div>
-                        <input type="text" placeholder="Username" v-model="userData.username" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
+                        <input type="text" placeholder="Pseudo" v-model="userData.username" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
                     </div>
                     <br>
                     <div>
@@ -18,16 +18,16 @@
                     </div>
                     <br>
                     <div>
-                        <input type="password" placeholder="Password" v-model="userData.password" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
+                        <input type="password" placeholder="Mot de passe" v-model="userData.password" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
                     </div>
                     <br>
                     <div>
-                        <input type="password" placeholder="Confirm password" v-model="userData.password2" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
+                        <input type="password" placeholder="Confirmer le mot de passe" v-model="userData.password2" class="mt-1 pl-3 block w-full border-none bg-l-background dark:bg-d-30 h-11 rounded-xl shadow-lg focus:border-d-10">
                     </div>
 
                     <div class="mt-7">
                         <button class="mb-4 bg-l-background dark:bg-d-30 border-l-10 dark:border-d-10 border-2 bottom-2 w-full py-3 rounded-xl shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
-                            Register
+                            Création du compte
                         </button>
                     </div>
                 </form>
