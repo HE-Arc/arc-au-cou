548e9522357786fdc211e0fc85ce56fb32f9cdd4
Tino3210 <izzovalentino0@gmail.com>
2022-03-31 15:16:10 +0200

    change axios baseUrl

--- arcaucou-front/nuxt.config.js
+++ arcaucou-front/nuxt.config.js
@@ -110,7 +110,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'http://arcaucou.srvz-webapp.he-arc.ch/api',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
